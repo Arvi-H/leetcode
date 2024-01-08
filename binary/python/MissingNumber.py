a359088b58fcdb1e5e6c12fef7fb0e5ef4b64389
Arvi-H <arvihaxhillari1@gmail.com>
2024-01-07 22:11:31 -0700

    clean start. day 0 leetcode journey

--- binary/python/MissingNumber.py
+++
@@ -1,7 +0,0 @@
-class MissingNumber(object):
-    def missing_number(nums):
-        n = len(nums)
-        expected_sum = n * (n+1) // 2 #Gauss's formula
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
-
--- binary/python/NumberOfOneBits.py
+++
@@ -1,9 +0,0 @@
-class NumberOfOneBits(object):
-    def hammingWeight(self, n):
-        count = 0
-    
-        while n:
-            count += n & 1
-            n >>= 1
-        
-        return count
