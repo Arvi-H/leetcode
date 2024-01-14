3e8672af05fb9972085b85cfcd2386a7d635443c
Arvi-H <arvihaxhillari1@gmail.com>
2024-01-14 14:08:44 -0700

    done

---
+++ ReversedLinkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr is not None:
+            # Reverse Direction
+            next = curr.next
+            curr.next = prev
+            
+            # Move pointers
+            prev = curr
+            curr = next
+        
+        return prev
