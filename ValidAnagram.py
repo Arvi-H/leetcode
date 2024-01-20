fb802778f98392e1c8ab3585efbea3b505c88583
Arvi-H <arvihaxhillari1@gmail.com>
2024-01-19 23:47:59 -0700

    Done

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,29 +1,24 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    
+    trackSLetters = {}
+    trackTLetters = {}
+    
+    for i in s:
+        if i in trackSLetters:
+            trackSLetters[i] += 1
+        else:
+            trackSLetters[i] = 1
+    
+    for i in t:
+        if i in trackTLetters:
+            trackTLetters[i] += 1
+        else:
+            trackTLetters[i] = 1
+
+    for i in trackSLetters:
+        if trackSLetters[i] != trackTLetters.get(i, 0):
             return False
         
-        trackSLetters = {}
-        trackTLetters = {}
-        
-        for i in s:
-            if i in trackSLetters:
-                trackSLetters[i] += 1
-            else:
-                trackSLetters[i] = 1
-        
-        for i in t:
-            if i in trackTLetters:
-                trackTLetters[i] += 1
-            else:
-                trackTLetters[i] = 1
-
-        for i in trackSLetters:
-            if trackSLetters[i] != trackTLetters.get(i, 0):
-                return False
-            
-        return True
-         
-solution = Solution()
-result = solution.isAnagram("abdsba", "abdsbb")
-print(result)
+    return True 
