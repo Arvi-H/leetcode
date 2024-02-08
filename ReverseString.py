ab6aa412ba2bc7799c22b1b7eaa06bdd598a19b1
Arvi-H <arvihaxhillari1@gmail.com>
2024-02-08 10:36:38 -0700

    done

--- ReverseString.py
+++ ReverseString.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def reverseString(self, s):
-        leftIndex, rightIndex = 0, len(s)-1
+def reverseString(s):
+    leftIndex, rightIndex = 0, len(s)-1
+    
+    while leftIndex < rightIndex:
+        s[leftIndex], s[rightIndex] = s[rightIndex], s[leftIndex]
+        leftIndex, rightIndex = leftIndex + 1, rightIndex - 1
         
-        while leftIndex < rightIndex:
-            s[leftIndex], s[rightIndex] = s[rightIndex], s[leftIndex]
-            leftIndex, rightIndex = leftIndex + 1, rightIndex - 1
-            
